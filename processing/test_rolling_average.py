68e0c71feb29acef56e42ba73e54893573a8c104
devarintideepthi26-tech <devarintideepthi26@gmail.com>
2026-08-10 12:03:09 +0530

    Expand rolling average test coverage

--- processing/test_rolling_average.py
+++ processing/test_rolling_average.py
@@ -13,6 +13,12 @@ class TestRollingAverage(unittest.TestCase):
     def test_decimal_average(self):
         self.assertEqual(calculate_average([10, 20, 25]), 18.33)
 
+    def test_negative_numbers(self):
+        self.assertEqual(calculate_average([-10, -20, -30]), -20.0)
+
+    def test_single_value(self):
+        self.assertEqual(calculate_average([50]), 50.0)
+
 
 if __name__ == "__main__":
     unittest.main()
